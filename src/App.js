17cfea88504ce84df34f79f93d1f591ca69c4ec5
Javier Zarate <zarate.javier.jz@gmail.com>
2020-10-13 20:58:06 -0600

    added react router and basic contact paeg

--- src/App.js
+++ src/App.js
@@ -1,12 +1,19 @@
 import React from 'react';
 import logo from './logo.svg';
 import './App.css';
-import Profile from './Components/Profile';
+import Home from './Components/Home';
+import Contact from './Components/Contact';
+import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
 
 function App() {
   return (
     <div className="app">
-      <Profile/>
+      <Router>
+        <Route>
+          <Home />
+        </Route>
+        <Contact />
+      </Router>
     </div>
   );
 }
