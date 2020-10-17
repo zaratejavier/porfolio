3dbee457a825bbf980493d9d874fdd4e22d441c9
Javier Zarate <zarate.javier.jz@gmail.com>
2020-10-16 20:05:00 -0600

    added a smooth scroll

--- src/Components/Header.js
+++ src/Components/Header.js
@@ -28,6 +28,36 @@ function Header() {
             <span className="header__optionLineTwo">Projects</span>
           </div>
         </Link> 
+
+        {/* <Link to={"#first"}>
+          <div
+            className="header__option">
+            <span className="header__optionLineTwo">first</span>
+          </div>
+        </Link>  */}
+
+        <div className="header__option">
+          {/* <span href="#third" className="header__optionLineTwo">third</span> */}
+          <a className="header__optionLineTwo" style={{ textDecoration: 'none', color: 'white' }} href="#first">First</a>
+        </div>
+
+        {/* <Link to={"#second"}>
+          <div
+            className="header__option">
+            <span className="header__optionLineTwo">second</span>
+          </div>
+        </Link>  */}
+
+        <div className="header__option">
+          {/* <span href="#third" className="header__optionLineTwo">third</span> */}
+          <a className="header__optionLineTwo" style={{ textDecoration: 'none', color: 'white' }} href="#second">Second</a>
+        </div>
+
+
+        <div className="header__option">
+          {/* <span href="#third" className="header__optionLineTwo">third</span> */}
+          <a className="header__optionLineTwo" style={{ textDecoration: 'none', color: 'white' }} href="#third">Third</a>
+        </div>
                 
       </div>
     </div>
--- src/Components/Home.js
+++ src/Components/Home.js
@@ -1,13 +1,65 @@
 import React from "react"
+import "../Styles/Home.css"
 
 const Home = () => {
   return (
-    <div className="home">
+    <section className="home">
       {/* Image */}
       <h1>Javier Zarate</h1>
+
+      <div id="first" style={{backgroundColor:"red", padding:"400px"}}>First section</div>
+
+      <div id="second" style={{backgroundColor:"blue", padding:"400px"}}>Second section</div>
+
+      <div id="third" style={{backgroundColor:"green", padding:"400px"}}>Third section</div>
+      
       {/* discription */}
-    </div>
+    </section>
   )
 }
 
 export default Home
+
+// a {
+//   display: inline-block;
+//   padding: 5px 7%;
+//   text-decoration: none;
+// }
+
+// nav, section {
+//   display: block;
+//   margin: 0 auto;
+//   text-align: center;
+// }
+
+// nav {
+//   width: 350px;
+//   padding: 5px;
+// }
+
+// section {
+//   width: 350px;
+//   height: 130px;
+//   overflow-y: scroll;
+//   border: 1px solid black;
+//   font-size: 0; 
+//   scroll-behavior: smooth;    /* <----- THE SECRET ---- */
+// }
+
+// section div{
+//   display: flex;
+//   align-items: center;
+//   justify-content: center;
+//   height: 100%;
+//   font-size: 8vw;
+// }
+// <nav>
+//   <a href="#page-1">1</a>
+//   <a href="#page-2">2</a>
+//   <a href="#page-3">3</a>
+// </nav>
+// <section>
+//   <div id="page-1">1</div>
+//   <div id="page-2">2</div>
+//   <div id="page-3">3</div>
+// </section>
