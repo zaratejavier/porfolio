d5132a73b0ffef5dc8614ac5af2305cc827ea9cb
Javier Zarate <zarate.javier.jz@gmail.com>
2020-11-20 12:52:44 -0700

    fixed header bug

--- src/Components/Header.js
+++ src/Components/Header.js
@@ -1,16 +1,14 @@
 import React from "react";
 import "../Styles/Header.css";
-import SearchIcon from "@material-ui/icons/Search";
-import ShoppingBasketIcon from "@material-ui/icons/ShoppingBasket";
 import { Link } from "react-router-dom";
 
 function Header() {  
   return (
-    <div className="header">
-      
+    <div className="header" >
+{/*       
         <div className="Header__name">
           <a className="header__logo" href="#About">
-            <span>Javier Zarate</span>
+            Javier Zarate
           </a>
         </div>
       
@@ -32,7 +30,25 @@ function Header() {
           <a className="header__optionLineTwo" style={{ textDecoration: 'none', color: 'white' }} href="#Contact">Contact</a>
         </div>    
         
-      </div>
+      </div> */}
+
+     <ul class="topnav">
+        <li>
+          <a className="header__logo" href="#About"><span>Javier Zarate</span></a>
+        </li>
+        <li className="right">
+          <a className="header__optionLineTwo" style={{ textDecoration: 'none', color: 'white' }} href="#Contact">Contact</a>
+        </li>
+        <li className="right">
+          <a className="header__optionLineTwo" style={{ textDecoration: 'none', color: 'white' }} href="#Portfolio">Portfolio</a>
+        </li>
+        <li className="right">
+          <a className="header__optionLineTwo" style={{ textDecoration: 'none', color: 'white' }} href="#Resume">Resume</a>
+        </li>
+        <li className="right">
+          <a className="header__optionLineTwo" style={{ textDecoration: 'none', color: 'white' }} href="#About">About</a>
+        </li>
+    </ul> 
     </div>
   );
 }
