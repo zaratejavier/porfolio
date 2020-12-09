690eff2504768493d83e78e25e72d37b541f177d
Javier Zarate <zarate.javier.jz@gmail.com>
2020-12-08 19:03:27 -0700

    added some style to the contact component

--- src/Components/Contact.js
+++ src/Components/Contact.js
@@ -4,10 +4,22 @@ import "../Styles/Contact.css"
 const Contact = () => {
   return (
     <div className="contact">
-      {/* <h3>Contact</h3> */}
+      {/* <div className="contact__title">Contact</div>
       <div>Shoot me an email or check out my online profiles</div>
-      <div>steve@stevejones.io</div>
+      <a className="contact__email" href="mailto:zarate.javier.jz@gmai.com">zarate.javier.jz@gmail.com</a>
+      <div className="contact__github">This website designed and developed by Javier Zarate View it on <a href="https://github.com/zaratejavier/porfolio">Github</a></div>
+      <div>© Javier Zarate 2020</div> */}
+
+      {/* <div> */}
+        <span>Contact</span>
+        <span>Shoot me an email or check out my online profiles</span>
+        <span><a className="contact__email" href="mailto:zarate.javier.jz@gmai.com">zarate.javier.jz@gmail.com</a></span>
+        <span>This website designed and developed by Javier Zarate View it on <a href="https://github.com/zaratejavier/porfolio">Github</a></span>
+        <span>© Javier Zarate 2020</span>
+      {/* </div> */}
     </div>
+
+
   )
 }
 
