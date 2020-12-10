59d65b01f37ffb174bb87a9b6ec413dfe4c5baa1
Javier Zarate <zarate.javier.jz@gmail.com>
2020-12-09 19:20:29 -0700

    added material UI

--- src/Components/Contact.js
+++ src/Components/Contact.js
@@ -1,22 +1,16 @@
 import React from "react"
 import "../Styles/Contact.css"
+import InstagramIcon from '@material-ui/icons/Instagram';
 
 const Contact = () => {
   return (
     <div className="contact">
-      {/* <div className="contact__title">Contact</div>
-      <div>Shoot me an email or check out my online profiles</div>
-      <a className="contact__email" href="mailto:zarate.javier.jz@gmai.com">zarate.javier.jz@gmail.com</a>
-      <div className="contact__github">This website designed and developed by Javier Zarate View it on <a href="https://github.com/zaratejavier/porfolio">Github</a></div>
-      <div>© Javier Zarate 2020</div> */}
-
-      {/* <div> */}
-        <span>Contact</span>
+        <span style={{fontSize: "50px"}}>Contact</span>
         <span>Shoot me an email or check out my online profiles</span>
         <span><a className="contact__email" href="mailto:zarate.javier.jz@gmai.com">zarate.javier.jz@gmail.com</a></span>
-        <span>This website designed and developed by Javier Zarate View it on <a href="https://github.com/zaratejavier/porfolio">Github</a></span>
+        <InstagramIcon/>
+        <span>This website designed and developed by Javier Zarate View it on <a href="https://github.com/zaratejavier/porfolio" target="_blank">Github</a></span>
         <span>© Javier Zarate 2020</span>
-      {/* </div> */}
     </div>
 
 
